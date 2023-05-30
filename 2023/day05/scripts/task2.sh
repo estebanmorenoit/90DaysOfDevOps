3339eb775a3db5a8cabfc7bf0125e443b345c913
estebanmorenoit <morenoramirezesteban@gmail.com>
2023-05-30 17:09:55 +0100

    Update

--- 2023/day05/scripts/task2.sh
+++ 2023/day05/scripts/task2.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-backup_dir="
+backup_dir="/Users/estareno/Documents/GitHub/90DaysOfDevOps"
+
+backup_name=backup_$(date)
+
+#https://www.linkedin.com/pulse/day-5-task-advanced-linux-shell-scripting-omkar-khedkar/
