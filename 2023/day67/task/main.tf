2b9fadc85e29b9c340a3b7d23490699a8375d0c8
estebanmorenoit <98739538+estebanmorenoit@users.noreply.github.com>
2023-07-20 18:22:33 +0100

    Day 67 added

--- 2023/day67/task/main.tf
+++ 2023/day67/task/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.8.0"
     }
   }
@@ -9,4 +9,53 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "devopschallenge-s3-bucket"
+}
+
+# Bucket policy to allow read-only access to the devops-user
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowPublicRead"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
+      },
+      {
+        Sid       = "AllowUserAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::974262444728:user/devops-user"
+        }
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# Allow public read acces
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
