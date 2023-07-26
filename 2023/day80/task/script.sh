6580df86e0ff81f22b946c3e7dc5b4c4c24ff97d
estebanmorenoit <morenoramirezesteban@gmail.com>
2023-07-26 19:39:57 +0100

    Day 80 and 81 update

---
+++ 2023/day80/task/script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Docker installation"
+echo "------------------------------------------------------------------"
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+
+echo "Jenkins installation"
+echo "------------------------------------------------------------------"
+sudo apt update
+sudo apt install -y openjdk-17-jre
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y jenkins
+
+echo "Add Jenkins to Docker Group"
+echo "------------------------------------------------------------------"
+sudo usermod -aG docker jenkins
