d09173d014494c8eae9b72a8a1e5620c76b78fc6
estebanmorenoit <98739538+estebanmorenoit@users.noreply.github.com>
2023-07-31 18:57:10 +0100

    Projects update

---
+++ 2023/day85/task/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "AWS CLI Installation"
+echo "------------------------------------------------------------------"
+sudo apt install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+echo "Docker installation"
+echo "------------------------------------------------------------------"
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+sudo reboot
